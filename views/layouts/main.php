84e99d31d91daae10f787cf3edb13db3f020c388
Victor Castiñeira <vancasti86@gmail.com>
2016-01-31 16:53:38 +0100

    Initial commit

---
+++ views/layouts/main.php
@@ -0,0 +1,76 @@
+<?php
+
+/* @var $this \yii\web\View */
+/* @var $content string */
+
+use yii\helpers\Html;
+use yii\bootstrap\Nav;
+use yii\bootstrap\NavBar;
+use yii\widgets\Breadcrumbs;
+use app\assets\AppAsset;
+
+AppAsset::register($this);
+?>
+<?php $this->beginPage() ?>
+<!DOCTYPE html>
+<html lang="<?= Yii::$app->language ?>">
+<head>
+    <meta charset="<?= Yii::$app->charset ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?= Html::csrfMetaTags() ?>
+    <title><?= Html::encode($this->title) ?></title>
+    <?php $this->head() ?>
+</head>
+<body>
+<?php $this->beginBody() ?>
+
+<div class="wrap">
+    <?php
+    NavBar::begin([
+        'brandLabel' => Html::img('img/logo_news.png'),
+        'brandUrl' => Yii::$app->homeUrl,
+        'options' => [
+            'class' => 'navbar-inverse navbar-fixed-top',
+        ],
+    ]);
+    echo Nav::widget([
+        'options' => ['class' => 'navbar-nav navbar-right'],
+        'items' => [
+            ['label' => 'Home', 'url' => ['/site/index']],
+            ['label' => 'Nosotros', 'url' => ['/site/about']],
+            ['label' => 'Contacto', 'url' => ['/site/contact']],
+            Yii::$app->user->isGuest ? (
+            ['label' => 'Entrar', 'url' => ['/site/login']]
+            ) : (
+                '<li>'
+                . Html::beginForm(['/site/logout'], 'post')
+                . Html::submitButton(
+                    'Logout (' . Yii::$app->user->identity->username . ')',
+                    ['class' => 'btn btn-link']
+                )
+                . Html::endForm()
+                . '</li>'
+            )
+        ],
+    ]);
+    NavBar::end();
+    ?>
+
+    <?= Breadcrumbs::widget([
+        'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
+    ]) ?>
+    <?= $content ?>
+</div>
+
+<footer class="footer">
+    <div class="container">
+        <p class="pull-left">&copy; <?= date('Y') ?>, Victor Castiñeira García - PHP Developer</p>
+
+        <p class="pull-right"><?= Yii::powered() ?></p>
+    </div>
+</footer>
+
+<?php $this->endBody() ?>
+</body>
+</html>
+<?php $this->endPage() ?>
---
+++ views/site/index.php
@@ -0,0 +1,168 @@
+<?php
+
+/* @var $this yii\web\View */
+
+use yii\data\ArrayDataProvider;
+
+$this->title = 'My Yii Application';
+?>
+<div class="site-index">
+
+    <div class="jumbotron">
+        <div class="box">
+            <h1>Información sobre vivienda y economía</h1>
+            <p class="lead">La actualidad inmobiliaria, económica y consejos sobre decoración </p>
+            <p><a class="btn btn-lg btn-pink" href="http://www.idealistanews.com">Entrar</a></p>
+        </div>
+    </div>
+    <div class="container">
+        <div class="body-content">
+
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="article">
+                        <h2>La economía española cerró 2015 con su mayor crecimiento desde 2007 con un 3,2%</h2>
+
+                        <p>El crecimiento del PIB español se concretó en un 3,2% al cierre de 2015, después del alza del
+                            0,8%
+                            registrado en el cuarto trimestre, según los datos provisionales publicados por el Instituto
+                            Nacional de
+                            Estadística (INE). Se trata de la cifra más alta desde 2007 y además coincide con el
+                            pronóstico del Banco de España. </p>
+                        <p><a class="btn btn-default"
+                              href="http://www.idealista.com/news/finanzas-personales/inversion/2016/01/29/740784-la-economia-espanola-cerro2015-con-su-mayor-crecimiento-desde-2007-con-un-3">Leer
+                                más</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <hr/>
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="article">
+                        <h2>Informe 'Inmuebles de alquiler' en Enero 2016</h2>
+                        <p>Datos tomados para la muestra</p>
+                        <?php
+                        $data = [
+                            '1' => [
+                                'id' => '1',
+                                'community' => 'Andalucía',
+                                'location' => '48',
+                                'property' => '15.990'
+                            ],
+                            '2' => [
+                                'id' => '2',
+                                'community' => 'Aragón',
+                                'location' => '3',
+                                'property' => '1.538'
+                            ],
+                            '3' => [
+                                'id' => '3',
+                                'community' => 'Extremadura',
+                                'location' => '5',
+                                'property' => '45'
+                            ],
+                            '4' => [
+                                'id' => '4',
+                                'community' => 'Madrid',
+                                'location' => '23',
+                                'property' => '21.538'
+                            ],
+                            '5' => [
+                                'id' => '5',
+                                'community' => 'Galicia',
+                                'location' => '16',
+                                'property' => '2.390'
+                            ],
+                        ];
+
+                        function filter($item)
+                        {
+                            $communityFilter = Yii::$app->request->getQueryParam('community');
+                            $locationFilter = Yii::$app->request->getQueryParam('location');
+                            $propertyFilter = Yii::$app->request->getQueryParam('property');
+
+                            $return = true;
+
+                            if (strlen($communityFilter) > 0) {
+                                if (strstr($item['community'], $communityFilter) !== false) {
+                                    $return *= TRUE;
+                                } else {
+                                    $return *= FALSE;
+                                }
+                            }
+
+                            if (strlen($locationFilter) > 0) {
+                                if ($item['location'] == $locationFilter) {
+                                    $return *= TRUE;
+                                } else {
+                                    $return *= FALSE;
+                                }
+                            }
+
+                            if (strlen($propertyFilter) > 0) {
+                                if ($item['property'] == $propertyFilter) {
+                                    $return *= TRUE;
+                                } else {
+                                    $return *= FALSE;
+                                }
+                            }
+                                return $return;
+                        }
+
+                        $filteredData = array_filter($data, 'filter');
+
+                        $provider = new ArrayDataProvider([
+                            'allModels' => $filteredData,
+                            'sort' => [
+                                'attributes' => ['id', 'community', 'location', 'property'],
+                            ],
+                            'pagination' => [
+                                'pageSize' => 10,
+                            ],
+                        ]);
+
+                        $users = $provider->getModels();
+                        $communityFilter = Yii::$app->request->getQueryParam('community', '');
+                        $locationFilter = Yii::$app->request->getQueryParam('location', '');
+                        $propertyFilter = Yii::$app->request->getQueryParam('property', '');
+                        $searchModel = [
+                            'id' => null,
+                            'community' => $communityFilter,
+                            'location' => $locationFilter,
+                            'property' => $propertyFilter
+                        ];
+                        ?>
+
+                        <?= \yii\grid\GridView::widget([
+                            'dataProvider' => $provider,
+                            'filterModel' => $searchModel,
+                            'columns' => [
+                                ['class' => 'yii\grid\SerialColumn'],
+                                [
+                                    'attribute' => 'community',
+                                    'filter' => '<input class="form-control" name="community" value="'. $searchModel['community'] .'" type="text">',
+                                    'value' => 'community',
+                                    'label' => 'Comunidad'
+                                ],
+                                [
+                                    'attribute' => 'location',
+                                    'filter' => '<input class="form-control" name="location" value="'. $searchModel['location'] .'" type="text">',
+                                    'value' => 'location',
+                                    'label' => 'Municipios'
+                                ],
+                                [
+                                    'attribute' => 'property',
+                                    'filter' => '<input class="form-control" name="property" value="'. $searchModel['property'] .'" type="text">',
+                                    'value' => 'property',
+                                    'label' => 'Inmuebles'
+                                ],
+                            ],
+                            'layout' => "{items}\n{pager}\n{summary}",
+                        ]) ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
